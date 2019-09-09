190015fa3e931b51ad4258f8a475fb50895aabb1
ashish060211 <ashish060211@gmail.com>
2019-09-09 22:03:58 +0530

    Chapter 6 : Using Error Boundaries (React 16+)

---
+++ my-app/src/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+    state = {
+        hasError: false,
+        error: ''
+    }
+    componentDidCatch = (error, info) => {
+        //console.log(info);
+        this.setState({
+            hasError: true,
+            error: error
+        });
+    }
+    render() {
+        if(this.state.hasError) {
+            return <h1>{this.state.error}</h1>;
+        } else {
+            return this.props.children;
+        }
+    }
+}
+
+export default ErrorBoundary;
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -4,6 +4,10 @@ import  classes from './Person.css';
 const person = (props) => {
     //return <p>I'm a Person and my age is {Math.floor(Math.random() * 30)} years old.</p>
     //return <p>I'm a {props.name} and my age is {props.age} years old.</p>
+    const rnd = Math.random();
+    if(rnd == 0.3) {
+        throw new Error('somethingwent wrong!!');
+    }
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm a {props.name} and my age is {props.age} years old.</p>
